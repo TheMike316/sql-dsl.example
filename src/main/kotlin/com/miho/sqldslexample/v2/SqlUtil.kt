bc04cf8d4f0f32e05f36aa7db62314fbc1a6d4e4
Michael Hornegger <michael.hornegger@karriere.at>
2019-02-06 07:28:33 +0100

    Add inline modifier to sql

--- src/main/kotlin/com/miho/sqldslexample/v2/SqlUtil.kt
+++ src/main/kotlin/com/miho/sqldslexample/v2/SqlUtil.kt
@@ -1,3 +1,3 @@
 package com.miho.sqldslexample.v2
 
-fun sql(block: SqlBuilder.() -> Unit) = SqlBuilder().apply(block).build()
+inline fun sql(block: SqlBuilder.() -> Unit) = SqlBuilder().apply(block).build()
